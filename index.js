603222381a7fa2dbe4ad7e2db84d4d4f0a3c2ba2
aperdizs <info@alvaroperdiz.com>
2020-10-30 12:54:52 +0100

    Expose logger

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const namespace = require('./src/namespace');
 const transactionId = require('./src/transaction-id');
+const logger = require('./src/logger');
 
 module.exports = {
   namespace,
   transactionId,
+  logger,
 }
