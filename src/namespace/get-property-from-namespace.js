19964bc7ea51d970d9f9af85b8f27e56e86c7508
Raul <rrequero@gmail.com>
2022-11-28 12:14:20 +0100

    API-425 add getTransactionId method

--- src/namespace/get-property-from-namespace.js
+++ src/namespace/get-property-from-namespace.js
@@ -1,10 +1,20 @@
 const { getNamespace } = require('cls-hooked');
+const {
+  TRANSACTION_ID: { NAMESPACE, PROPERTY },
+} = require('../constant');
 
 const getPropertyFromNamespace = (namespace, property) => {
-    const myRequest = getNamespace(namespace);
-    return myRequest && myRequest.get(property) ? myRequest.get(property) : undefined;
+  const myRequest = getNamespace(namespace);
+  return myRequest && myRequest.get(property)
+    ? myRequest.get(property)
+    : undefined;
+};
+
+const getTransactionId = () => {
+  getPropertyFromNamespace(NAMESPACE, PROPERTY);
 };
 
 module.exports = {
-    getPropertyFromNamespace,
-}
+  getPropertyFromNamespace,
+  getTransactionId,
+};
